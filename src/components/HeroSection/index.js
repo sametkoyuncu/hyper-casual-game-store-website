91061ce444e052cab8d32dcb3ffbe3323ecce68b
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-15 14:56:08 +0300

    Add old project to react

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <section id="hero">
+      <div className="container col-xxl-8 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5">
+          <div className="col-10 col-sm-8 col-lg-6">
+            <img
+              src="/img/hyper-casual-game-phone.png"
+              className="d-block mx-lg-auto img-fluid"
+              alt="Bootstrap Themes"
+              width="700"
+              height="500"
+              loading="lazy"
+            />
+          </div>
+          <div className="col-lg-6">
+            <div className="display-5 lh-1 mb-3">
+              <span className="fw-bold">Hello world</span> 👋
+            </div>
+            <h3>Welcome to my game universe.</h3>
+            <p className="lead">
+              This journey is about making hyper casual games with using pure
+              JavaScript.
+            </p>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+              <a
+                href="https://github.com/sametkoyuncu"
+                target="_blank"
+                type="button"
+                className="btn btn-primary btn-lg px-4 me-md-2 btn-purple rounded-pill"
+              >
+                Github
+              </a>
+              <a
+                href="https://www.linkedin.com/in/samet-koyuncu/"
+                target="_blank"
+                type="button"
+                className="btn btn-outline-primary btn-lg px-4 btn-outline-purple rounded-pill"
+              >
+                LinkedIn
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HeroSection
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"
+      aria-label="navbar"
+    >
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          sk.
+        </a>
+        <button
+          className="navbar-toggler collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbar"
+          aria-controls="navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="navbar-collapse collapse" id="navbar">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Games
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
