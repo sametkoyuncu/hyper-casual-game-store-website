a358cdd7c11c2de519c801871b250239e86f997e
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-29 00:02:56 +0300

    Add hero section and game section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import HeroSectionTailwind from './components/HeroSectionTailwind'
-import Navbar from './components/Navbar'
-import GameSection from './components/GameSection'
+import GameSectionTailwind from './components/GameSectionTailwind'
 
 import './FoodThemeCss.css'
 import './App.css'
@@ -10,6 +9,27 @@ function App() {
   return (
     <div className="App">
       <HeroSectionTailwind />
+      <div id="games">
+        <GameSectionTailwind
+          index={0}
+          bgColor="indigo"
+          title="Doodle Jump"
+          description="Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium."
+          image="img/games/doodle-jump.png"
+          buttonGithub="https://github.com/sametkoyuncu/hyper-casual-games-with-js/tree/1-doodle-jump"
+          buttonPlay="https://github.com/sametkoyuncu/hyper-casual-games-with-js/tree/1-doodle-jump"
+        />
+
+        <GameSectionTailwind
+          index={1}
+          bgColor="red"
+          title="Nokia 3310 Snake Game"
+          description="Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium."
+          image="img/games/snake.png"
+          buttonGithub="https://github.com/sametkoyuncu/hyper-casual-games-with-js/tree/2-snake-nokia-3310/snakeNokia3310"
+          buttonPlay="https://github.com/sametkoyuncu/hyper-casual-games-with-js/tree/2-snake-nokia-3310/snakeNokia3310"
+        />
+      </div>
     </div>
   )
 }
