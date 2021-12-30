bd2bdfcd722176bd681e4c361ff5edb4d3ebe915
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-30 23:24:45 +0300

    Add dark theme to hero section

--- src/components/HeroSectionTailwind/index.js
+++ src/components/HeroSectionTailwind/index.js
@@ -1,28 +1,18 @@
 export default function Example() {
   return (
-    <div className="relative bg-white overflow-hidden">
+    <div className="relative bg-white dark:bg-zinc-800 overflow-hidden">
       <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <svg
-            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-            fill="currentColor"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg>
-
-          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <div className="relative z-10 pb-8 bg-white dark:bg-zinc-800 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <main className="pt-10 mx-auto max-w-7xl px-4 sm:pt-12 sm:px-6 md:pt-16 lg:pt-20 lg:px-8">
             <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white sm:text-5xl md:text-6xl">
                 <span className="block xl:inline">Hello, world! </span>
                 <br />
                 <span className="block text-2xl sm:text-4xl md:text-5xl text-indigo-600 xl:inline">
                   Welcome to my game universe.
                 </span>
               </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              <p className="mt-3 text-base text-gray-500 dark:text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 This journey is about making hyper casual games with using pure
                 JavaScript.
               </p>
@@ -51,7 +41,7 @@ export default function Example() {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <div className="bg-indigo-600/50">
           <img
-            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full lg:inset-y-0"
             src="/img/hero-img.png"
             alt=""
           />
